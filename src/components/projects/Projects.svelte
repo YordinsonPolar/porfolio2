859bd3d1afa70f5e7d3c4cf1c831c8313e3363e5
unknown <rune.escape888@gmail.com>
2020-11-24 18:29:12 -0400

    add: projects layout

--- src/components/projects/Projects.svelte
+++ src/components/projects/Projects.svelte
@@ -1,11 +1,38 @@
 <script>
-	
+	import Line from '../Line.svelte';
+
+	const projectsDelay = 5;
 </script>
 
 <section class="projects">
 	<div class="projects-wrapper">
 		<div class="header">
-			<h1 class="title">Projects</h1>
+			<h1 class="title">
+				<Line
+				  time="var(--transition-time)"
+				  color="var(--primary)" 
+					distance="100%" 
+					position="bottom" 
+					origin="left" 
+					volumen="4px" delay="calc({projectsDelay + .6} * var(--transition-time))" />
+				<Line
+				  time="var(--transition-time)"
+				  color="var(--primary)" 
+					distance="100%" 
+					position="right" 
+					origin="bottom" 
+					volumen="4px" delay="calc({projectsDelay + 1.2} * var(--transition-time))" />
+				Projects
+			</h1>
+		</div>
+		<div class="grid">
+			<div class="item"></div>
+			<div class="item"></div>
+			<div class="item"></div>
+			<div class="item"></div>
+			<div class="item"></div>
+			<div class="item"></div>
+			<div class="item"></div>
 		</div>
 	</div>
 </section>
@@ -16,23 +43,35 @@
 		font-size: var(--title-size);
 		position: relative;
 		width: 100%;
-		min-height: 100vh;
+		min-height: 100%;
 		display: grid;
 		align-items: center;
+		margin: 50px 0;
 	}
-
 
 	.projects .projects-wrapper {
 		margin: 0 auto;
 		width: 90%;
-		height: 90vh;
+		height: 100%;
 	}
 
 	.projects .header .title {
+		position: relative;
 		display: inline;
-		padding: 10px 25px;
+		padding: 20px 35px;
 		font-size: var(--title-size);
-		border-bottom: var(--line-volumen) solid var(--primary);
-		border-right: var(--line-volumen) solid var(--primary);
+	}
+
+
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(4,1fr);
+		grid-gap: 20px;
+		margin-top: 50px;
+	}
+
+	.item {
+		background: darkCyan;
+		height: 450px;
 	}
 </style>
