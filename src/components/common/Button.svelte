87ffbcdc4e944d94ed78f8e77376248321e5d957
unknown <rune.escape888@gmail.com>
2020-11-25 09:10:05 -0400

    fix: button component is now more responsive

--- src/components/common/Button.svelte
+++ src/components/common/Button.svelte
@@ -1,16 +1,19 @@
 <script>
 	export let label;
+	export let size;
 </script>
 
-<button>{label}</button>
+<button style={`--size: ${size}`}>{label}</button>
 
 <style>
 	button {
+		--size: 14px;
 		background: none;
 		color: #fff;
-		font-size: 2rem;
-		padding: 5px 35px;
-		border: 4px solid var(--primary);
+		font-weight: 500;
+		font-size: var(--size);
+		padding: 6px 30px;
+		border: calc( var(--size) / 5) solid var(--primary);
 		transform: skewX(-20deg);
 	}
 </style>
