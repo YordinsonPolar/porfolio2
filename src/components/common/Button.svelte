a99210831f6311ccf32c358c149c8de5b0fbefa3
unknown <rune.escape888@gmail.com>
2020-11-25 08:59:25 -0400

    add: common components

---
+++ src/components/common/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let label;
+</script>
+
+<button>{label}</button>
+
+<style>
+	button {
+		background: none;
+		color: #fff;
+		font-size: 2rem;
+		padding: 5px 35px;
+		border: 4px solid var(--primary);
+		transform: skewX(-20deg);
+	}
+</style>
